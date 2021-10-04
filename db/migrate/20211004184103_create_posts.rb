e754db5a70b28a68e177193af91fecad1bd4c1fc
rickychow2006 <rickychow0716@gmail.com>
2021-10-04 13:47:45 -0500

    imported bootstrap, created initial db for users,posts,comments, and services

--- db/migrate/20211004184103_create_posts.rb
+++ db/migrate/20211004184103_create_posts.rb
@@ -1,6 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
+      t.string :title
+      t.text :content
+      t.belongs_to :user, foreign_key: true
+      t.belongs_to :service, foreign_key: true
 
       t.timestamps
     end
