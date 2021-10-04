b399ce31e7c42dadfda5244d75ef12a0142a7d32
rickychow2006 <rickychow0716@gmail.com>
2021-10-04 13:53:07 -0500

    initial set up on model files

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+    belongs_to :user 
+    belongs_to :service
+    has_many :comments 
+    has_many :users, through :comments 
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    has_many :posts
+    has_many :comments
+    has_many :commented_posts, through: :comments, source: :post
+    has_secure_password 
+    has_many :services, through: :posts
+  
+    validates :username, :email, presence: true
 end
