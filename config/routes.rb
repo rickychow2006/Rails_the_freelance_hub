62eb97f4f333dfb4a402c66df7977a35a219e4a0
rickychow2006 <rickychow0716@gmail.com>
2021-10-19 10:45:08 -0500

    added omniauth and search routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,24 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/' => 'sessions#home'
+  root "sessions#home"
+
+  get '/' => 'sessions#show'
   post '/' => 'sessions#create'
   
+  match '/auth/:provider/callback' => 'sessions#google',  via: [:get, :post]
+
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
-
   
   resources :sessions
   resources :services
   resources :users
   resources :posts 
-  resources :userfavorites, only: [:create, :destroy]
+  resources :userfavorites
   resources :users do 
     resources :posts
-    end 
-
+    resources :userfavorites
+  end 
+  
+  get '/search', to: 'users#search', as: 'search'
+  
 end
