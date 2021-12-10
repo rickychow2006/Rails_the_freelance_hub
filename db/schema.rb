3ffc62b146f4cbf74301566754fba30a2b8565a4
rickychow2006 <rickychow0716@gmail.com>
2021-12-10 16:11:44 -0600

    changed db to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_10_06_213057) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
