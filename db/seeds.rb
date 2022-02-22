7fff93675dcb006f8b7898954b8321b0622e073c
rickychow2006 <rickychow0716@gmail.com>
2022-02-21 23:00:45 -0600

    updated webpack

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Service.destroy_all
+User.destroy_all
+Post.destroy_all
+
+
 Service.create(name: "Graphic & Design")
 Service.create(name: "Digitial Marketing")
 Service.create(name: "Writing & Translation")
